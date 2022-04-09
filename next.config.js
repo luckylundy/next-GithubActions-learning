03965d6b7fecc23774a15324f4da8ad71d7ea803
luckylundy <kamon3027@gmail.com>
2022-04-09 18:15:26 +0900

    confirmation

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  // some configuration
+  assetPrefix: process.env.NODE_ENV === "production" ? "/project_name" : "",
+  // another configuration
+};
